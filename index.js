bbcf52e5ceb631e220f8f65dcdfe177436d0ab01
Albert4940 <dorcealbertmary@gmail.com>
2023-08-09 22:19:27 -0400

    Setup database connection

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 require('dotenv').config()
+
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Database connection successful');
+})
+.catch((err) => {
+  console.error('Database connection error');
+});
 
 app.use(cors())
 app.use(express.static('public'))
